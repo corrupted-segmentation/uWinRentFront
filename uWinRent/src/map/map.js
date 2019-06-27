1c46ebba48c3d2166fda1649789457f42f268c13
prairir <prairie421@gmail.com>
2019-06-26 20:22:34 -0400

    have map tile up. have to change the token as there is no easy way to load this one without a server

--- uWinRent/src/map/map.js
+++ uWinRent/src/map/map.js
@@ -1,6 +1,13 @@
 import * as L from 'leaflet/src/Leaflet' 
 
 export function makeMap(id){
-    var mymap = L.map(id).setView([51.505, -0.09], 13);
+    var mymap = L.map(id).setView([42.306, -83.067], 15);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoicHJhaXJpciIsImEiOiJjanhkdzFlem4waWYxM3htbXNneGF1ZG5oIn0.cQBLSxaasBom95pejfIbIQ'
+}).addTo(mymap);
     return mymap;
 }
